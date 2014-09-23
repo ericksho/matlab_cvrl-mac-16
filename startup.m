7b3193d3d92291dfdab8e16ab95be703d4895ab1
ericksho <evsvec@uc.cl>
2014-09-23 11:57:15 -0400

    bugs arreglados

--- startup.m
+++ startup.m
@@ -1,1 +1,8 @@
-run vlfeat-0.9.19/toolbox/vl_setup
+%seteamos vlfeat
+run('vlfeat-0.9.19/toolbox/vl_setup')
+%seteamos spams
+run('spams-matlab/start_spams')
+%agregamos balu al path
+addpath(genpath('Balu'));
+%agregamos bsif al path
+addpath(genpath('bsif_code_and_data'));
